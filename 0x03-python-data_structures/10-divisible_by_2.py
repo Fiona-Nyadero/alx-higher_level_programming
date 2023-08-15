d26c7d74b2d571261fb203e58963aa37f99aa9b5
Fiona-Nyadero <fionanyadero@gmail.com>
2023-08-15 02:44:41 +0000

    Task10 corr

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
 
     mults = []
-    for numb in my_list:
+    for numb in range(len(my_list)):
         if my_list[numb] % 2 == 0:
             mults.append(True)
         else:
