6c01a26763bfec482e8fafc6c048a644491043a4
Fiona-Nyadero <fionanyadero@gmail.com>
2023-08-31 01:06:15 +0000

    Tasks 7n8

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""Linked list definition"""
+
+
+class Node:
+    """This class reps a Node"""
+    def __init__(self, data, next_node=None):
+        """
+        Inits a new Node object.
+
+        Args:
+            data (int): The data in the neu node.
+            next_node (Node): The next linked node. Defaults to None.
+        """
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """returns(int) the data stored in the node"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Sets the data stored in the node.
+
+        Args:
+            value(int): New node value.
+
+        Raises:
+            TypeError: If value != int.
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """returns(Node) the next node in the list"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets the next node in the list.
+
+        Args:
+            value(Node): Neu next node.
+
+        Raises:
+            TypeError: If value != Node/None.
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """This class reps a singly-linked list"""
+
+    def __init__(self):
+        """Inits a new SinglyLinkedList object(PIA)"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Inserts neu Node into the sorted pst in the list(increase).
+
+        Args:
+            value(int): The value to insert.
+        """
+        neu_node = Node(value)
+
+        if self.__head is None:
+            self.__head = neu_node
+            neu_node.next_node = None
+        elif value < self.__head.data:
+            neu_node.next_node = self.__head
+            self.__head = neu_node
+        else:
+            bucket = self.__head
+            while (bucket.next_node is not None and
+                    bucket.next_node.data < value):
+                bucket = bucket.next_node
+            neu_node.next_node = bucket.next_node
+            bucket.next_node = neu_node
+
+    def __str__(self):
+        """Returns a string rep of the linked list(str)"""
+        res = ""
+        bucket = self.__head
+        while bucket is not None:
+            res += str(bucket.data) + "\n"
+            bucket = bucket.next_node
+        return res
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """This class reps a square shape."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Inits a private instance attribute.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+            position (tuple):Pstn of the printsquare dfaults to (0, 0).
+            """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """return(int) new sq size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets Sq size
+
+        Args:
+            value (int): size
+        Raises:
+            TypeError: If size != integer.
+            ValueError: If size < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """return new sq position(tuple)"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets Sq position
+
+        Args:
+            value (tuple): sq position
+        Raises:
+            TypeError: If the position != tuple of 2 +ve int.
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """return sq area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square using '#'"""
+        if self.__size == 0:
+            print("")
+            return
+
+        for y in range(self.__position[1]):
+            print()
+
+        for y in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Str rep for Square objects"""
+        sq_str = ""
+        if self.__size == 0:
+            return sq_str
+
+        sq_str += "\n" * self.__position[1]
+        sq_str += "\n".join([" " * self.__position[0] +
+                            "#" * self.__size] * self.__size)
+
+        return sq_str
