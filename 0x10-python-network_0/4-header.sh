518752a2ba3784cea23efee2457d5f4002f39130
Fiona-Nyadero <fionanyadero@gmail.com>
2024-01-11 02:56:02 +0000

    Tasks 1-5 cor

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # Sends a GET request, with custom header and response body
-
-# Check for a URL argument
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-resbody=$(curl -sH "X-School-User-Id: 98" "$1")
-echo "$resbody"
+curl -sH "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # Sends POST request (specified parameters) & displays the response body
-
-# Check for URL argument
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-email="test@gmail.com"
-subject="I will always be here for PLD"
-post_resp=$(curl -sX POST -d "email=$email&subject=$subject" "$1")
-echo "$post_resp"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
