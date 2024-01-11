757f327e17da7cf6ece03efb17ee1387729164b7
Fiona-Nyadero <fionanyadero@gmail.com>
2024-01-11 02:31:49 -0800

    Task 8 Corr

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request and displays the response body
-curl -sX POST -H "Content-Type: application/json" -d "$json_content" "$1"
+curl -sX POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
